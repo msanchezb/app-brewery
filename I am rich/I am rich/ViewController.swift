185800f7b9388d5204ebfa57bf7b16010c045e9f
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-09-13 01:10:30 +0200

    initial commit

---
+++ I am rich/I am rich/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I am rich
+//
+//  Created by María Sánchez Barrueta on 10/9/18.
+//  Copyright © 2018 Maria Sanchez Barrueta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
