741cb874ad72f6a6fe8d9bb76958f7f596e0b9fb
Maria SanchezBarrueta <mc.sanchez.4142@gmail.com>
2018-09-13 14:43:56 +0200

    started new app

---
+++ Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,14 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+var myAge: Int = 11
+
+myAge = 12
+
+let myName : String = "Maria"
+
+let myFullName = myName + " Sanchez"
+
+let details : String = "\(myName) Sanchez \(myAge)"
